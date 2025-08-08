bd426df819e09b914bdf981b4403cc53c4f1c35b
Brian Stubbs <stentroad@gmail.com>
2025-08-08 18:36:29 -0300

    fix: phoenix 1.8 workaround removed Phoenix.Endpoint.Supervisor.config/2

--- lib/beacon/private.ex
+++ lib/beacon/private.ex
@@ -53,7 +53,12 @@ defmodule Beacon.Private do
   end
 
   def endpoint_config(otp_app, endpoint) do
-    Phoenix.Endpoint.Supervisor.config(otp_app, endpoint)
+    # Phoenix.Endpoint.Supervisor.config/2 was removed in phoenix 1.8.x
+    if Kernel.function_exported?(Phoenix.Endpoint.Supervisor, :config, 2) do
+      Phoenix.Endpoint.Supervisor.config(otp_app, endpoint)
+    else
+      Application.get_env(otp_app, endpoint, [])
+    end
   end
 
   def endpoint_host(otp_app, endpoint) do
