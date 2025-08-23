09b7417e1575fec4b3f98e1ec69807f7821ca3dc
Brian Stubbs <stentroad@gmail.com>
2025-08-23 20:06:00 -0300

    fix warning about Beacon.Plug is missing from the Router pipeline.
    I think commit 2b961784fc11fbf3ee9367a729300665b8f9ee59 broke this
    causing the following warning to be generated:
    
    [warning] Beacon.Plug is missing from the Router pipeline.
    
    Page Variants will not be used.

--- lib/beacon/plug.ex
+++ lib/beacon/plug.ex
@@ -38,7 +38,9 @@ defmodule Beacon.Plug do
   defp put_roll(conn) do
     path_list = conn.path_params["path"]
 
-    with %{private: %{phoenix_live_view: {_, _, %{extra: %{session: %{"beacon_site" => site}}}}}} <- conn,
+    # I think commit 2b961784fc11fbf3ee9367a729300665b8f9ee59 broke this...
+    # with %{private: %{phoenix_live_view: {_, _, %{extra: %{session: %{"beacon_site" => site}}}}}} <- conn,
+    with %{private: %{phoenix_live_view: {_, _, %{extra: %{session: {Beacon.Router, :session, [site, _]}}}}}} <- conn,
          {_, _} <- Beacon.RouterServer.lookup_path(site, path_list, 1) do
       Plug.Conn.put_session(conn, "beacon_variant_roll", Enum.random(1..100))
     else
